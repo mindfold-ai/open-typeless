8612c19e7803b77c904fd02a0b4e220ce5975a6e
taosu <taosu@mindfold.ai>
2026-01-29 18:39:51 +0800

    feat(asr): asr-audio-recorder

---
+++ src/renderer/src/modules/asr/hooks/index.ts
@@ -0,0 +1,6 @@
+/**
+ * ASR React hooks exports.
+ */
+
+export { useAudioRecorder } from './useAudioRecorder';
+export type { UseAudioRecorderReturn } from './useAudioRecorder';
---
+++ src/renderer/src/modules/asr/lib/index.ts
@@ -0,0 +1,6 @@
+/**
+ * ASR library exports.
+ */
+
+export { float32ToInt16, int16ToArrayBuffer, float32ToArrayBuffer } from './pcm-converter';
+export { AudioRecorder } from './audio-recorder';
